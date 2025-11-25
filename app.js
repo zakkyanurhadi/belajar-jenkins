0c806c19003708913bb4c85c78e959b7b039a8b0
zakkyanurhadi <zakkyanurhadi@gmail.com>
2025-11-26 03:51:09 +0700

    Upload project jenkins pertama

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("--- MULAI APLIKASI ---");
+console.log("Aplikasi ini berjalan di Jenkins Freestyle Project.");
+console.log("Tanggal Build: " + new Date().toString());
+console.log("--- SELESAI ---");
