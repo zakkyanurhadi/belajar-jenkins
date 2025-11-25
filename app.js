2b71001762251b32b563a8be52b9c82c00dcc5d5
zakkyanurhadi <zakkyanurhadi@gmail.com>
2025-11-26 04:21:56 +0700

    test

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-console.log("--- MULAI APLIKASI ---");
-console.log("Aplikasi ini berjalan di Jenkins Freestyle Project.");
-console.log("Tanggal Build: " + new Date().toString());
-console.log("--- SELESAI ---");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div style="font-family: sans-serif; text-align: center; padding: 20px;">
+        <h1>🚀 Aplikasi Berhasil Deployed!</h1>
+        <p>Halo dari Jenkins Freestyle Project + Docker.</p>
+        <hr>
+        <p>Waktu Server: <strong>${new Date().toString()}</strong></p>
+    </div>
+  `);
+});
+
+// Endpoint khusus untuk testing otomatis
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+// Kita export servernya agar bisa dimatikan (close) oleh script lain
+const server = app.listen(port, () => {
+  console.log(`Webserver berjalan di http://localhost:${port}`);
+});
+
+module.exports = server;
